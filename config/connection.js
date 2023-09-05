70bb6c49378ee6d5c0e1045b2c7a32bcf2be0ee3
bpoole53 <bpoole53@gmail.com>
2023-09-05 00:05:21 -0500

    created an INSERT for the add employee option

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Bootcamp1!",
-  database: "employees"
+  database: "employees_db"
 });
 
 connection.connect(function (err) {
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const connection = require("./config/connection");
 const { listAllDepartments } = require("./lib/queries")
 const { listAllEmployees } = require("./lib/queries")
 const { listAllRoles } = require("./lib/queries")
+const { addEmployee } = require("./lib/queries")
 const { displayAllDepartments } = require("./lib/displays")
 /*
   There are a lot of menu items presented to users in this app. The only real way you cam manage them 
@@ -38,11 +39,66 @@ function start(){
         });
         break;
       case "Add Employee":
-        listAllDepartments().then( ([rows]) => {
-          displayAllDepartments(rows);
-          start();
-        });
-        break;
+        inquirer.prompt([
+          {   
+            type: "prompt",
+            message: "What is the employee's first name?",
+            name: "fname",
+          },
+          {   
+            type: "prompt",
+            message: "What is the employee's last name?",
+            name: "lname",
+          },
+          {   
+            type: "list",
+            message: "What is the Employee's Role?",
+            name: "role",
+            choices: [
+              {
+                name: 'Sales',
+                value: 1
+              },
+              {
+                name: 'Engineering',
+                value: 2,
+              },
+              {
+                name: 'Finance',
+                value: 3,
+              },
+              {
+                name: 'Legal',
+                value: 4,
+              },
+            ]},
+            {
+            type: "list",
+            message: "Who is the employee's manager?",
+            name: "manager",
+            choices: [
+              {
+                name: 'Ted Gatsby',
+                value: 1
+              },
+              {
+                name: 'None',
+                value: 2,
+              },
+              {
+                name: 'Alicia Silverstone',
+                value: 3,
+              },
+              {
+                name: 'Dag swaniels',
+                value: 4,
+              },
+            ]},
+            ]).then (response => {
+              addEmployee(response);
+              start();
+            });                 
+        break;        
       case "Update Employee Role":
         listAllDepartments().then( ([rows]) => {
           displayAllDepartments(rows);
